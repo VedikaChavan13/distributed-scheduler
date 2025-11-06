426d23c987c6bab8728ffb8229484af38d73f30b
VedikaChavan13 <vedikachavan1301@gmail.com>
2025-11-05 19:04:29 -0500

    Update: JUnit 4.13.2 and basic DAG scheduler unit test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,37 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.example.model.TaskNode;
+import org.example.model.WorkflowDAG;
+import org.example.model.TopoSort;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import java.util.*;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+
+    @Test
+    public void testTopologicalSortOrder() {
+        // Create a sample workflow DAG
+        TaskNode t1 = new TaskNode("task1", Arrays.asList());
+        TaskNode t2 = new TaskNode("task2", Arrays.asList("task1"));
+        TaskNode t3 = new TaskNode("task3", Arrays.asList("task1"));
+        TaskNode t4 = new TaskNode("task4", Arrays.asList("task2", "task3"));
+
+        Map<String, TaskNode> taskMap = new HashMap<>();
+        taskMap.put(t1.getId(), t1);
+        taskMap.put(t2.getId(), t2);
+        taskMap.put(t3.getId(), t3);
+        taskMap.put(t4.getId(), t4);
+
+        WorkflowDAG workflow = new WorkflowDAG("demoWorkflow", taskMap);
+        List<TaskNode> sorted = TopoSort.sortTasks(workflow.getTasks());
+
+        // Topo order: t1 before t2 and t3; t2 and t3 before t4
+        assertTrue(sorted.indexOf(t1) < sorted.indexOf(t2));
+        assertTrue(sorted.indexOf(t1) < sorted.indexOf(t3));
+        assertTrue(sorted.indexOf(t2) < sorted.indexOf(t4));
+        assertTrue(sorted.indexOf(t3) < sorted.indexOf(t4));
     }
 }
